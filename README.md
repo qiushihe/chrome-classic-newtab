b130c5a428115166855ba1dacb5adfc5deff518f
Billy He <qiushihe@me.com>
2013-10-19 23:36:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Chrome Classic New Tab
+
+Replaces your New Tab page with what resembles the classic New Tab page of the olden days.
+
+### License
+
+Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+
+For more information, please see http://creativecommons.org/licenses/by-sa/3.0
---
+++ newtab.js
@@ -0,0 +1,111 @@
+(function() {
+
+  var bookmarksList = document.getElementById("bookmarks");
+  var othersLink = document.getElementById("others");
+  var appsWrapper = document.getElementById("apps-wrapper");
+  var appsList = document.getElementById("apps");
+
+  var favicon = function(url) {
+    return "chrome://favicon/" + url;
+  };
+
+  var appIcon = function(icons) {
+    var size = 0;
+    var url = null;
+    var i; for (i = 0; i < icons.length; i++) {
+      var icon = icons[i];
+      if (icon.size > size) {
+        size = icon.size;
+        url = icon.url;
+      }
+    }
+    return url;
+  };
+
+  var bookmarkItem = function(bookmark) {
+    var item = document.createElement("li");
+    var link = document.createElement("a");
+    var icon = document.createElement("img");
+    var label = document.createElement("span");
+
+    link.className = "clearfix";
+    link.setAttribute("href", bookmark.url);
+
+    icon.setAttribute("src", favicon(bookmark.url));
+    icon.setAttribute("width", "16");
+    icon.setAttribute("height", "16");
+
+    label.innerHTML = bookmark.title.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    link.appendChild(icon);
+    link.appendChild(label);
+    item.appendChild(link);
+
+    return item;
+  };
+
+  var populateBookmarksBar = function(bookmarks) {
+    var i; for (i = 0; i < bookmarks.length; i++) {
+      bookmarksList.appendChild(bookmarkItem(bookmarks[i]));
+    }
+  };
+
+  var populateOtherBookmarks = function(bookmarks) {
+
+  };
+
+  var appItem = function(app) {
+    var item = document.createElement("li");
+    var link = document.createElement("a");
+    var icon = document.createElement("img");
+    var label = document.createElement("span");
+
+    link.className = "clearfix";
+    link.addEventListener('click', function() {
+      chrome.management.launchApp(app.id, function() {
+        window.close();
+      });
+    }, false);
+
+    icon.setAttribute("src", appIcon(app.icons));
+    icon.setAttribute("width", "128");
+    icon.setAttribute("height", "128");
+
+    label.innerHTML = app.name.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+    link.appendChild(icon);
+    link.appendChild(label);
+    item.appendChild(link);
+
+    return item;
+  };
+
+  var populateApps = function(apps) {
+    var i; for (i = 0; i < apps.length; i++) {
+      appsList.appendChild(appItem(apps[i]));
+    }
+  };
+
+  chrome.bookmarks.getChildren("1", function(bookmarks) {
+    populateBookmarksBar(bookmarks);
+  });
+
+  chrome.management.getAll(function(extensions) {
+    var apps = [];
+    var i; for (i = 0; i < extensions.length; i++) {
+      var extension = extensions[i];
+      if (extension.enabled && extension.isApp) {
+        apps.push(extension);
+      }
+    }
+    populateApps(apps);
+    onWindowResize();
+  });
+
+  var onWindowResize = function() {
+    appsList.style.marginTop = ((appsWrapper.clientHeight - appsList.clientHeight) / 3) + "px";
+  };
+
+  window.onresize = onWindowResize;
+
+}).call(this);
